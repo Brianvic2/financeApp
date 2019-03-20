11247b082fdbe1d834ef6a90fb395f5c5a568c9d
BRIAN VICTOR OGAGA <brivicogaga@gmail.com>
2019-03-20 22:30:33 +0300

    Add files via upload

---
+++ StockApp/index.js
@@ -0,0 +1,63 @@
+/*import React from "react"
+import ReactDOM from "react-dom"
+import MyInfo from "./components/MyInfo"
+
+import App from "./App"
+ReactDOM.render(<App/>,document.getElementById("root"))
+*/
+/*
+ReactDOM.render(<App/>,document.getElementById("root"))
+*/
+
+
+/*
+function App() {
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    
+    if (hours < 12) {
+      timeOfDay = "MORINING"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "AFTERNOON"
+    } else {
+      timeOfDay = "NIGHT"
+    }
+    const styles ={
+        color: "#FF8C00",
+        backgroundColor: "#FF2D00",
+        fontSize:30,
+        padding:40,
+        textAlign: 'center',
+        border: '5px solid pink'
+
+    
+
+    }
+    return (
+      <h1 style={styles}>GOOD {timeOfDay}!</h1>
+    )
+  }
+  */
+  
+
+import React from "react"
+import ReactDOM from "react-dom"
+//import App from './TodoAPP/App'
+ 
+//import Button from "./Button/Button";
+ 
+//import Tick from "./State_pract/Tick"
+//import App from './Conditional/App'
+//import IsLogged from './Conditional/IsLogged'
+//import App from './Starwars/App'
+//import NameForm from './Form/App'
+
+//import App from './Form/App'
+//import Basic from './Basic/Basic'
+//import App from './FormEx/App'
+//import Calculator from './TEMPCALC/Calculator'
+
+import App from './StockApp/App'
+ReactDOM.render(<App/>, document.getElementById('root'))
+ 
